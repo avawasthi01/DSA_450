58a0cf056a3111618874f9cc95a7baa02d59fb7c
avawasthi01 <102598778+avawasthi01@users.noreply.github.com>
2022-03-29 19:01:05 +0530

    comments refractored

--- Reverse_the_array.py
+++ Reverse_the_array.py
@@ -1,9 +1,8 @@
 ### Given an array (or string), the task is to reverse the array/string.
-"""Input  : arr[] = {1, 2, 3}
-Output : arr[] = {3, 2, 1}
+###Input  : arr[] = {1, 2, 3}
+###Output : arr[] = {3, 2, 1}
 
-Input :  arr[] = {4, 5, 1, 2}
-Output : arr[] = {2, 1, 5, 4}"""
+
 def reverse_it(s,arr):
     rev_str = s[::-1]
     rev_arr = arr[::-1]
