76bd54917bf79f7aaa3145d15decde5811fce637
avawasthi01 <102598778+avawasthi01@users.noreply.github.com>
2022-03-29 18:59:18 +0530

    Create Reverse_the_array.py

---
+++ Reverse_the_array.py
@@ -0,0 +1,18 @@
+### Given an array (or string), the task is to reverse the array/string.
+"""Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}"""
+def reverse_it(s,arr):
+    rev_str = s[::-1]
+    rev_arr = arr[::-1]
+    return rev_str,rev_arr
+
+
+
+
+if __name__ == '__main__':
+    s=input()  #string
+    arr=[int(i) for i in input().split()]
+    print(reverse_it(s,arr))
