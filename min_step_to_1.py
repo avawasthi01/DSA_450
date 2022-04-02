710319f79146e3f13f54a36e0c2b559f06521213
avawasthi01 <102598778+avawasthi01@users.noreply.github.com>
2022-04-02 10:45:17 +0530

    dp

--- min_step_to_1.py
+++ min_step_to_1.py
@@ -30,8 +30,38 @@ def countMinStepsToOne(n,l) :
 
 
 
+
+def min_step_to_1_dp(n,l) :
+    if n==0 or n==1:
+        return 0
+    if n==2 or n==3:
+        return 1
+    l[0]=0
+    l[1]=1
+    l[2]=1
+    l[3]=1
+    for j in range(4,n+1):
+        x=l[j-1]
+        #print(x,"x")
+        y=n+1
+        z=n+1
+        if j%2==0:
+            y=l[j//2]
+        if j%3==0:
+            z=l[j//3]
+        a=min(y,z)
+        l[j]=min(a,x)+1
+        #print(l[j],j)
+    return l[n]
+
+
+
+
+
+
 #main
 n = int(stdin.readline().rstrip())
 l=[]
 l=[-1]*(n+1)
 print(countMinStepsToOne(n,l))
+print(min_step_to_1_dp(n,l))
